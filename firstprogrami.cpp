de8868626933c5a304e1cc494fd7f3011bfd48c4
Rodolfo Castro <soyracm@gmail.com>
2025-04-01 16:12:36 -0400

    test

--- firstprogrami.cpp
+++ firstprogrami.cpp
@@ -4,18 +4,24 @@ using namespace std;
 
 int, bool, float, ..., void
 
+las funciones void NO devuelven ningun tipo de dato!
 
-dato nombreFuncion (entradas) {
-	
+ejemplo de funcion void:
+
+void imprimir () {
+}
+
+
+dato nombreFuncion (entradas) {	
 
 }
+
 Ejemplo:
 
 int sumarValores (int a, int b) {
 
 	return(a + b);
 }
-
 
 */
 
@@ -25,13 +31,13 @@ int sumarValores (int a, int b) {
 int main() {
 	
 //    Primer ejercico
-    float numero1 = 0, numero2 = 0;
-    cout << "Ingresa los numeros 1 y 2 separados por un espacio" << endl;
-    cin >> numero1 >> numero2;
-    cout << "La suma de los valores 1 y 2 es: " << numero1 + numero2 << endl;
-    cout << "La resta de los valores 1 y 2 es: " << numero1 - numero2 << endl;
-    cout << "La multiplicacion de los valores 1 y 2 es: " << numero1 * numero2 << endl;
-    cout << "La division de los valores 1 y 2 es: " << numero1 / numero2 << endl;
+//    float numero1 = 0, numero2 = 0;
+//    cout << "Ingresa los numeros 1 y 2 separados por un espacio" << endl;
+//    cin >> numero1 >> numero2;
+//    cout << "La suma de los valores 1 y 2 es: " << numero1 + numero2 << endl;
+//    cout << "La resta de los valores 1 y 2 es: " << numero1 - numero2 << endl;
+//    cout << "La multiplicacion de los valores 1 y 2 es: " << numero1 * numero2 << endl;
+//    cout << "La division de los valores 1 y 2 es: " << numero1 / numero2 << endl;
 
 return 0;
 }
