ffe2bff21b0e526055ad0bc772b5ca2276bbdfad
Rodolfo Castro <soyracm@gmail.com>
2025-04-01 16:02:51 -0400

    test

--- firstprogrami.cpp
+++ firstprogrami.cpp
@@ -1,12 +1,38 @@
-#include<iostream> // IN OUT Stream -> library
+#include <iostream> // IN OUT Stream -> library
 using namespace std;
+/*
 
-int main ()
-{
-	cout<< " HOLA MUNDO ";
-	system("Title MI PRIMER PROGRAMA");
-	system("color 3b");
-	system("PAUSE");
-	return 0;
+int, bool, float, ..., void
+
+
+dato nombreFuncion (entradas) {
+	
+
+}
+Ejemplo:
+
+int sumarValores (int a, int b) {
+
+	return(a + b);
+}
+
+
+*/
+
+
+
+
+int main() {
+	
+//    Primer ejercico
+    float numero1 = 0, numero2 = 0;
+    cout << "Ingresa los numeros 1 y 2 separados por un espacio" << endl;
+    cin >> numero1 >> numero2;
+    cout << "La suma de los valores 1 y 2 es: " << numero1 + numero2 << endl;
+    cout << "La resta de los valores 1 y 2 es: " << numero1 - numero2 << endl;
+    cout << "La multiplicacion de los valores 1 y 2 es: " << numero1 * numero2 << endl;
+    cout << "La division de los valores 1 y 2 es: " << numero1 / numero2 << endl;
+
+return 0;
 }
 
