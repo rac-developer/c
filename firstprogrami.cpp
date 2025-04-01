245945765ffb46dac9235252d4570406ce9a8913
Rodolfo Castro <soyracm@gmail.com>
2025-04-01 12:05:17 -0400

    first commit

---
+++ firstprogrami.cpp
@@ -0,0 +1,12 @@
+#include<iostream> // IN OUT Stream -> library
+using namespace std;
+
+int main ()
+{
+	cout<< " HOLA MUNDO ";
+	system("Title MI PRIMER PROGRAMA");
+	system("color 3b");
+	system("PAUSE");
+	return 0;
+}
+
