e9deadc502590849ede52fc4fef1a6c4b5ca3ff3
Alejandro Castro <soyracm@gmail.com>
2025-09-20 21:53:47 -0400

    changes

---
+++ 00-test/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main () {
+	
+	// float puede tener cierta cantidad de numero con comas
+	float num = 2.573;
+	float num2 = 3.456;
+		
+	cout << num+num2 << endl;
+	
+	// Para que no se cierre el programa
+	getch();
+	return 0;
+}
